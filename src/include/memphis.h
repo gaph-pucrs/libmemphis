c7f8d7d12890ff312cd6cd0e59e0b0e1029ea84a
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2024-04-15 17:24:42 -0300

    fix BrNoC parameters

--- src/include/memphis.h
+++ src/include/memphis.h
@@ -19,8 +19,8 @@
 static const unsigned MEMPHIS_KERNEL_MSG = 0x10000000;
 static const unsigned MEMPHIS_FORCE_PORT = 0x80000000;
 
-static const int BR_SVC_TGT = 5;
-static const int BR_SVC_ALL = 6;
+static const int BR_SVC_ALL = 0;
+static const int BR_SVC_TGT = 2;
 
 enum _syscall {
 	SYS_writepipe = 1,
